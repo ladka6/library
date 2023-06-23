0d56c1f273c17183fe5d0060cd0378c8435ab5cc
Ege Erdal <ege@inspakt.com>
2023-06-23 20:02:14 +0300

    code improvements

--- library_app/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace library_app.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
